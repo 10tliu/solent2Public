c8d35dee5ec4ea6bd46b080306806227257d8c1d
10tliu <3liut48@solent.ac.uk>
2018-10-01 12:57:31 +0100

    first session of week 2

---
+++ myPracticeCourseWork/week2/factoryandfacade/factoryandfacade/src/main/java/org/solent/com504/factoryandfacade/model/FarmFacadeImpl.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.solent.com504.factoryandfacade.model;
+
+import java.util.List;
+
+/**
+ *
+ * @author 3liut48
+ */
+public class FarmFacadeImpl implements FarmFacade{
+
+    public List<Animal> getAllAnimals() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void addDog(String name) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void addCat(String name) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void addCow(String name) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
